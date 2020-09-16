54d9fe0c09162c85375d41b4ab040497e0e83c01
Mike Mindel <mike@webventurer.com>
2020-09-16 17:38:07 +0100

    Integrate  method

--- src/bottles.py
+++ src/bottles.py
@@ -25,19 +25,21 @@ class Bottles:
         if number == 0:
             return "no more"
         else:
-            return number
+            return str(number)
 
     def verse(self, number):
         if number == 0:
             return (
-                "No more bottles of beer on the wall, "
-                "no more bottles of beer.\n"
-                "Go to the store and buy some more, "
-                "99 bottles of beer on the wall.\n"
+                f"{self.quantity(number).capitalize()} bottles "
+                f"of beer on the wall, "
+                f"no more bottles of beer.\n"
+                f"Go to the store and buy some more, "
+                f"99 bottles of beer on the wall.\n"
             )
         else:
             return (
-                f"{number} {self.container(number)} of beer on the wall, "
+                f"{self.quantity(number).capitalize()} "
+                f"{self.container(number)} of beer on the wall, "
                 f"{number} {self.container(number)} of beer.\n"
                 f"Take {self.pronoun(number)} down and pass it around, "
                 f"{self.quantity(number-1)} {self.container(number-1)} "
